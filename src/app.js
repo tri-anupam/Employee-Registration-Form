e7768b49cd1d563212f73c742731ff973e9a46f0
Anupam Tripathi <arvindtri.70@gmail.com>
2023-01-29 23:05:34 +0530

    hashing is completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
-const path = require("path")
 const app = express();
 const port = process.env.PORT || 3000;
+const bcrypt = require("bcryptjs")
 
 
 //Requiring database
@@ -12,61 +12,80 @@ require("./db/conn")
 
 //for json data on pages
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 //serving files ejs
 //loading static file
 // app.use('../public',express.static('public'));
-app.set("view engine","ejs");
-app.use(express.static(__dirname+'../asset'))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '../asset'))
 
 // get index page
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index");
 })
 
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     res.render("register");
 })
 
-app.post("/register",async (req,res)=>{
-    try{
+//registration page
+app.post("/register", async (req, res) => {
+    try {
         const password = req.body.password;
         const cpassword = req.body.confirmpassword;
-        if(password === cpassword){
+        if (password === cpassword) {
             const employeeData = new Employee(req.body);
             const createEmployeeData = await employeeData.save();
             res.status(201).render("login")
-        }else{
+        } else {
             res.send("password not matched.")
         }
-    }catch(e){
+    } catch (e) {
         res.status(400).send(e);
     }
 })
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login");
 })
 
-app.post("/login",async (req,res)=>{
-    try{
+//login page
+app.post("/login", async (req, res) => {
+    try {
         const email = req.body.email;
         const password = req.body.password;
 
-        const userEmail = await Employee.findOne({email:email});
-        
-        // console.log(userEmail,userEmail.password);
-        if(userEmail.password === password){
+        const userEmail = await Employee.findOne({ email: email });
+
+        const isMatch = await bcrypt.compare(password, userEmail.password)
+
+        if (isMatch) {
             res.status(201).send("You are successfully logged in. ")
-        }else{
+        } else {
             res.send("emailid or password doesn't matched.")
         }
-    }catch(e){
+    } catch (e) {
         res.status(500).send(e)
     }
 })
 
-app.listen(port,()=>{
+
+// const bcrypt = require("bcryptjs")
+
+// const securePassword = async (password) =>{
+
+//     const passwordHash = await bcrypt.hash(password,10)
+//     console.log(passwordHash);
+
+//     const passwordMatch = await bcrypt.compare(password,passwordHash)
+//     console.log(passwordMatch);
+// }
+
+// securePassword("anupam")
+
+
+
+app.listen(port, () => {
     console.log(`Connection successfull at port ${port}`);
 })
--- src/models/registration.js
+++ src/models/registration.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 const employeeSchema = new mongoose.Schema({
     firstname:{
@@ -35,6 +36,19 @@ const employeeSchema = new mongoose.Schema({
         type:String,
         required:true
     }
+})
+
+employeeSchema.pre("save",async function(next){
+   if(this.isModified("password")){
+
+    // console.log(`the current password is ${this.password}`);
+    this.password = await bcrypt.hash(this.password, 10)
+    // console.log(`the current password is ${this.password}`);
+
+    this.confirmpassword = undefined;
+   }
+    
+    next();
 })
 
 //creation of collection
